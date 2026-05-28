a61ae2025b4c34621dd5f4f157708bae842d7135
Alexandra2873 <alexandranur2007@gmail.com>
2026-05-28 15:55:39 +0500

    Update пр_9_10.cpp

--- пр_9_10.cpp
+++ пр_9_10.cpp
@@ -1,18 +1,21 @@
-//вариант 18
+/*Вариант 18.
+Определить структурный тип, описывающий компьютерные игры (название игры,
+студия разработчик, стоимость подписки, объем аудитории игроков,
+поддерживаемые платформы: PC, mobile, gamepad; тип: RPG, стратегия, гонки,
+авиасимулятор и др.). Заполнить структурный массив 20-ю записями. Переписать из
+исходного массива в другой массив, информацию только об авиасимуляторах с
+аудиторией от 1 миллиона пользователей. Затем новый массив отсортировать по
+поддерживаемым платформам. Вывести все данные по конкретной игре. Вывести 3
+самых дорогих игр. Реализовать функцию изменения игры. В отдельный массив
+поместить все стратегии для PC. Реализовать вывод отфильтрованных данных в виде
+оберточной функции.*/
+
 
 #include <iostream>
 #include <fstream>
-using namespace std;
 #include <string>
 
-
-enum Genre {
-    rpg,
-    strategy,
-    gonki,
-    aviasim,
-    drugie
-};
+using namespace std;
 
 struct Platforms {
     bool pc;
@@ -21,239 +24,215 @@ struct Platforms {
 };
 
 struct Game {
-    string name;
+    string name;    
     string razrab;
     float price;
     int auditoria;
     Platforms platforms;
-    Genre genre;
+    string genre; 
 };
 
-int countPlatforms(Platforms p) {
-    int count = 0;
-    if (p.pc) count++;
-    if (p.mobile) count++;
-    if (p.gamepad) count++;
-    return count;
-}
-
-void printGame(const Game& g) {
-    cout << "Название: " << g.name << endl;
-    cout << "Студия: " << g.razrab << endl;
-    cout << "Цена: " << g.price << endl;
-    cout << "Аудитория: " << g.auditoria << " млн" << endl;
-    cout << "Платформы: ";
+void printgame(Game g) {
+    cout << "Название: " << g.name << ", Студия: " << g.razrab << endl;
+    cout << "Цена: " << g.price << ", Аудитория: " << g.auditoria << " млн" << endl;
+    cout << "Жанр: " << g.genre << ", Платформы: ";
     if (g.platforms.pc) cout << "PC ";
     if (g.platforms.mobile) cout << "Mobile ";
     if (g.platforms.gamepad) cout << "Gamepad ";
     cout << endl;
 }
 
-void print(Game g[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << "\nИгра " << i+1 << endl;
-        cout << "Название: " << g[i].name << endl;
-        cout << "Студия: " << g[i].razrab << endl;
-        cout << "Цена: " << g[i].price << endl;
-        cout << "Аудитория: " << g[i].auditoria << " млн" << endl;
-        cout << "Платформ: " << countPlatforms(g[i].platforms) << endl;
-    }
-}
-
-void puzirokSort(Game g[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (countPlatforms(g[j].platforms) < countPlatforms(g[j+1].platforms)) {
-                Game temp = g[j];
-                g[j] = g[j+1];
-                g[j+1] = temp;
-            }
-        }
-    }
-}
-
-void changeGame(Game& g, string newName, float newPrice) {
+void change(Game &g, string newName, float newPrice) {
     g.name = newName;
     g.price = newPrice;
 }
 
-
-
-void save(Game games[], int length) {
-    ofstream fout("games.txt");
-    for (int i = 0; i < length; i++) {
-        fout << games[i].name << " | Разработчик: " << games[i].razrab << " | Цена: " << games[i].price << " | Аудитория: " << games[i].auditoria << " млн" << endl;
-    }
-    fout.close();
-    cout << "Структура записана в файл games.txt" << endl;
+int countplatforms(Platforms p) {
+    int count = 0;
+    if (p.pc) count = count + 1;
+    if (p.mobile) count = count + 1;
+    if (p.gamepad) count = count + 1;
+    return count;
 }
 
+void printfiltr(Game filterarr[], int count, string title) {
+    cout << endl;
+    cout << " " << title << endl << endl;
+    
+    if (count == 0) {
+        cout << "нет данных" << endl;
+        return;
+    }
+    for (int i = 0; i < count; i++) {
+        printgame(filterarr[i]);
+    }
+}
 
-void read() {
-    string str;
-    ifstream fin("games.txt");
+// запись в бинарный файл
+void savebin(Game g[], int n) {
+    ofstream fout("games.bin", ios::binary);
+    for (int i = 0; i < n; i++) {
+       
+        fout.write((char*)&g[i].price, sizeof(g[i].price));
+        fout.write((char*)&g[i].auditoria, sizeof(g[i].auditoria));
+        fout.write((char*)&g[i].platforms, sizeof(g[i].platforms));
+        
+        int nameLen = g[i].name.length();
+        fout.write((char*)&nameLen, sizeof(nameLen));
+        fout.write(g[i].name.c_str(), nameLen);
+
+        int razrabLen = g[i].razrab.length();
+        fout.write((char*)&razrabLen, sizeof(razrabLen));
+        fout.write(g[i].razrab.c_str(), razrabLen);
+
+        int genreLen = g[i].genre.length();
+        fout.write((char*)&genreLen, sizeof(genreLen));
+        fout.write(g[i].genre.c_str(), genreLen);
+    }
+    fout.close();
+}
+
+//  чтение из бинарного файла
+void readbin(Game g[], int n) {
+    ifstream fin("games.bin", ios::binary);
     if (fin.is_open()) {
-        cout << "Файл прочитан:" << endl;
-        while (!fin.eof()) {
-            getline(fin, str);
-            cout << str << endl;
+        for (int i = 0; i < n; i++) {
+            fin.read((char*)&g[i].price, sizeof(g[i].price));
+            fin.read((char*)&g[i].auditoria, sizeof(g[i].auditoria));
+            fin.read((char*)&g[i].platforms, sizeof(g[i].platforms));
+            
+            int nameLen;
+            fin.read((char*)&nameLen, sizeof(nameLen));
+            g[i].name.resize(nameLen);
+            fin.read(&g[i].name[0], nameLen);
+
+            int razrabLen;
+            fin.read((char*)&razrabLen, sizeof(razrabLen));
+            g[i].razrab.resize(razrabLen);
+            fin.read(&g[i].razrab[0], razrabLen);
+
+            int genreLen;
+            fin.read((char*)&genreLen, sizeof(genreLen));
+            g[i].genre.resize(genreLen);
+            fin.read(&g[i].genre[0], genreLen);
+        }
+        fin.close();
+    }
+}
+
+void savetext(Game g[], int n) {
+    ofstream fout("price.txt");
+    for (int i = 0; i < n; i++) {
+        fout << g[i].price << endl;
+    }
+    fout.close();
+}
+
+void readtext(Game g[], int n) {
+    ifstream fin("price.txt");
+    float p;
+    int i = 0;
+    while (fin >> p) {
+        if (i < n) {
+            g[i].price = p;
+            i = i + 1;
         }
     }
     fin.close();
+    cout << "Цены обновлены из файла" << endl;
 }
-
-
-void save_bin(Game games[], int length) {
-    ofstream fout("games.bin", ios::binary | ios::out);
-    if (!fout.is_open()) return;
-
-    for (int i = 0; i < length; i++) {
-        
-        size_t str_size = games[i].name.size();
-        fout.write((char*)&str_size, sizeof(str_size));
-        fout.write(games[i].name.c_str(), str_size);
-
-        size_t razrab_size = games[i].razrab.size();
-        fout.write((char*)&razrab_size, sizeof(razrab_size));
-        fout.write(games[i].razrab.c_str(), razrab_size);
-
-        fout.write((char*)&games[i].price, sizeof(games[i].price));
-        fout.write((char*)&games[i].auditoria, sizeof(games[i].auditoria));
-        fout.write((char*)&games[i].platforms, sizeof(games[i].platforms));
-        fout.write((char*)&games[i].genre, sizeof(games[i].genre));
-    }
-    fout.close();
-    cout << "Бинарный файл записан." << endl;
-}
-
-
-void read_bin(int length) {
-    ifstream fin("games.bin", ios::binary | ios::in);
-    if (!fin.is_open()) return;
-
-    cout << "Чтение из бинарного файла" << endl;
-    for (int i = 0; i < length; i++) {
-        Game temp;
-        size_t str_size;
-
-        fin.read((char*)&str_size, sizeof(str_size));
-        char* buffer = new char[str_size + 1];
-        fin.read(buffer, str_size);
-        buffer[str_size] = '\0';
-        temp.name = buffer;
-        delete[] buffer;
-
-        fin.read((char*)&str_size, sizeof(str_size));
-        buffer = new char[str_size + 1];
-        fin.read(buffer, str_size);
-        buffer[str_size] = '\0';
-        temp.razrab = buffer;
-        delete[] buffer;
-
-        fin.read((char*)&temp.price, sizeof(temp.price));
-        fin.read((char*)&temp.auditoria, sizeof(temp.auditoria));
-        fin.read((char*)&temp.platforms, sizeof(temp.platforms));
-        fin.read((char*)&temp.genre, sizeof(temp.genre));
-
-        printGame(temp);
-        cout << endl;
-    }
-    fin.close();
-}
-
 
 int main() {
+    setlocale(LC_ALL, "ru");
+
     Game games[20] = {
-        {"Microsoft Flight Simulator", "Asobo Studio", 9.99, 2, {1,0,1}, aviasim},
-        {"IL-2 Sturmovik", "1C Game Studios", 0, 1, {1,0,1}, aviasim},
-        {"DCS World", "Eagle Dynamics", 0, 3, {1,0,1}, aviasim},
-        {"X-Plane 12", "Laminar Research", 0, 1, {1,0,1}, aviasim},
-        {"War Thunder", "Gaijin Entertainment", 0, 25, {1,1,1}, aviasim},
-        {"StarCraft II", "Blizzard Entertainment", 0, 6, {1,0,0}, strategy},
-        {"Age of Empires IV", "Relic Entertainment", 0, 5, {1,0,0}, strategy},
-        {"Civilization VI", "Firaxis Games", 29.99, 8, {1,1,1}, strategy},
-        {"Total War: Three Kingdoms", "Creative Assembly", 0, 3, {1,0,0}, strategy},
-        {"Company of Heroes 3", "Relic Entertainment", 0, 2, {1,0,1}, strategy},
-        {"Cyberpunk 2077", "CD Projekt Red", 0, 18, {1,0,1}, rpg},
-        {"The Witcher 3", "CD Projekt Red", 0, 30, {1,1,1}, rpg},
-        {"Forza Horizon 5", "Playground Games", 0, 12, {1,0,1}, gonki},
-        {"Genshin Impact", "miHoYo", 4.99, 60, {1,1,1}, rpg},
-        {"Minecraft", "Mojang Studios", 0, 140, {1,1,1}, drugie},
-        {"Among Us", "Innersloth", 0, 50, {1,1,1}, drugie},
-        {"FIFA 23", "EA Sports", 4.99, 20, {1,1,1}, drugie},
-        {"Call of Duty", "Activision", 0, 40, {1,1,1}, drugie},
-        {"Apex Legends", "Respawn Entertainment", 0, 35, {1,1,1}, drugie},
-        {"Fortnite", "Epic Games", 0, 80, {1,1,1}, drugie}
+        {"Microsoft Flight Simulator", "Asobo Studio", 59.9, 2, {1,0,1}, "авиасимулятор"},
+        {"IL-2 Sturmovik", "1C Game Studios", 39.9, 1, {1,0,0}, "авиасимулятор"},
+        {"DCS World", "Eagle Dynamics", 10.0, 3, {1,0,1}, "авиасимулятор"},
+        {"X-Plane 12", "Laminar Research", 49.9, 1, {1,0,1}, "авиасимулятор"},
+        {"War Thunder", "Gaijin", 0, 25, {1,1,1}, "авиасимулятор"},
+        {"StarCraft II", "Blizzard", 0, 6, {1,0,0}, "стратегия"},
+        {"Age of Empires IV", "Relic", 39.9, 5, {1,0,0}, "стратегия"},
+        {"Civilization VI", "Firaxis", 29.9, 8, {1,1,1}, "стратегия"},
+        {"Total War", "Creative Assembly", 49.9, 3, {1,0,0}, "стратегия"},
+        {"Company of Heroes 3", "Relic", 59.9, 2, {1,0,1}, "стратегия"},
+        {"Cyberpunk 2077", "CD Projekt", 50.0, 18, {1,0,1}, "RPG"},
+        {"The Witcher 3", "CD Projekt", 29.9, 30, {1,1,1}, "RPG"},
+        {"Forza Horizon 5", "Playground", 49.9, 12, {1,0,1}, "гонки"},
+        {"Genshin Impact", "miHoYo", 0, 60, {1,1,1}, "RPG"},
+        {"Minecraft", "Mojang", 26.0, 140, {1,1,1}, "другое"},
+        {"Among Us", "Innersloth", 4.9, 50, {1,1,1}, "другое"},
+        {"FIFA 23", "EA Sports", 69.9, 20, {1,1,1}, "другое"},
+        {"Call of Duty", "Activision", 69.9, 40, {1,1,1}, "другое"},
+        {"Apex Legends", "Respawn", 0, 35, {1,1,1}, "другое"},
+        {"Fortnite", "Epic Games", 0, 80, {1,1,1}, "другое"}
     };
 
-
-    save(games, 20);
-    cout << endl << endl;
-    read();
-    cout << endl << endl;
-    save_bin(games, 20);
-    cout << endl << endl;
-    read_bin(20);
-    cout << endl << endl;
-
-
-    Game avia[20];                //авиасимуляторы с аудиторией >= 1 млн
-    int aviaсount = 0;
+    Game gamesbin[20];
     
-    for (int i = 0; i < 20; i++) {
-        if (games[i].genre == aviasim && games[i].auditoria >= 1) {
-            avia[aviaсount] = games[i];
-            aviaсount++;
-        }
+    savebin(games, 20);
+    readbin(gamesbin, 20);
+    
+    savetext(gamesbin, 20);
+    readtext(gamesbin, 20);
+
+    cout << "\nвывод конкретной игры" << endl;
+    printgame(gamesbin[10]); 
+
+    Game puzirok[20];
+    for(int i = 0; i < 20; i++) {
+        puzirok[i] = gamesbin[i];
     }
-
-    puzirokSort(avia, aviaсount);          //сорт по платформам
-
-
-    cout << endl << "Вывод данных по конкретной игре (первой):" << endl;
-    printGame(games[0]);
-
-
-
-    Game copy[20];
-    for (int i = 0; i < 20; i++) copy[i] = games[i];
     
     for (int i = 0; i < 19; i++) {
         for (int j = 0; j < 19 - i; j++) {
-            if (copy[j].price < copy[j+1].price) {
-                Game temp = copy[j];
-                copy[j] = copy[j+1];
-                copy[j+1] = temp;
+            if (puzirok[j].price < puzirok[j+1].price) {
+                Game temp = puzirok[j];
+                puzirok[j] = puzirok[j+1];
+                puzirok[j+1] = temp;
             }
         }
     }
     
-    cout << endl << "3 самых дорогих игр:" << endl;
+    cout << "\nтоп-3 дорогих игр" << endl;
     for (int i = 0; i < 3; i++) {
-        cout << copy[i].name << " - " << copy[i].price << endl;
+        cout << i+1 << ". " << puzirok[i].name << puzirok[i].price << " $" << endl;
     }
 
-    cout << endl << "Вывод структуры после изменения игры:" << endl;
-    changeGame(games[0], "Flight Simulator 2024", 12.99);
-    printGame(games[0]);
+    cout << "\nпосле изменения" << endl;
+    change(gamesbin[0], "Flight Simulator 2024", 79.99);
+    printgame(gamesbin[0]);
 
-
-
-    Game strategii[20];
-    int strategсount = 0;
-    
+    Game aviaarr[20];
+    int aviacount = 0;
     for (int i = 0; i < 20; i++) {
-        if (games[i].genre == strategy && games[i].platforms.pc == true) {
-            strategii[strategсount] = games[i];
-            strategсount++;
+        if (gamesbin[i].genre == "авиасимулятор" && gamesbin[i].auditoria >= 1) {
+            aviaarr[aviacount] = gamesbin[i];
+            aviacount = aviacount + 1;
         }
     }
 
-    cout << endl << "Вывод структуры после сортировки по платформам:" << endl;
-    print(avia, aviaсount);
+    for (int i = 0; i < aviacount - 1; i++) {
+        for (int j = 0; j < aviacount - i - 1; j++) {
+            if (countplatforms(aviaarr[j].platforms) < countplatforms(aviaarr[j+1].platforms)) {
+                Game temp = aviaarr[j];
+                aviaarr[j] = aviaarr[j+1];
+                aviaarr[j+1] = temp;
+            }
+        }
+    }
 
-    cout << endl << "Вывод всех стратегий для PC:" << endl;
-    print(strategii, strategсount);
-    
+    Game strategarr[20];
+    int strategyCount = 0;
+    for (int i = 0; i < 20; i++) {
+        if (gamesbin[i].genre == "стратегия" && gamesbin[i].platforms.pc == true) {
+            strategarr[strategyCount] = gamesbin[i];
+            strategyCount = strategyCount + 1;
+        }
+    }
+
+    printfiltr(aviaarr, aviacount, "авиасимуляторы от 1 млн (отсортированы)");
+    printfiltr(strategarr, strategyCount, "стратегии для PC");
+
     return 0;
 }
